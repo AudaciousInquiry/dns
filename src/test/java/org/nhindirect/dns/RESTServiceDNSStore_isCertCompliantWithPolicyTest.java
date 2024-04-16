ed420285df00e6087c39034d848d39c84630157c
Austin Moody <austin.moody@gmail.com>
2024-04-16 15:10:13 -0400

    Update org.mockito.Matchers to org.mockito.ArgumentMatchers

--- src/test/java/org/nhindirect/dns/RESTServiceDNSStore_isCertCompliantWithPolicyTest.java
+++ src/test/java/org/nhindirect/dns/RESTServiceDNSStore_isCertCompliantWithPolicyTest.java
@@ -6,7 +6,7 @@ import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertNull;
 import static org.junit.Assert.assertTrue;
-import static org.mockito.Matchers.any;
+import static org.mockito.ArgumentMatchers.any;
 
 import java.security.cert.X509Certificate;
 
